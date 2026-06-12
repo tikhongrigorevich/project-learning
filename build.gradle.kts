cddab12de030c1213bd8b0142a6912d6f15f898c
Tikhon Pilipchuk <tikhon.pilipchuk@gmail.com>
2026-06-12 13:15:36 +0300

    update build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:6.1.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.apache.commons:commons-lang3:3.20.0")
